8f597734fa031a98e5f0a969daf14a1cdcca02f2
Anthony DiRusso <ossurid@gmail.com>
2019-03-31 10:56:19 -0400

    adds db content

--- schema.sql
+++ schema.sql
@@ -1,1 +1,6 @@
 --schema.sql--
+create table five_random_gary_busey_movies (
+    id serial primary key,
+    title varchar(300),
+    year_released integer
+);
--- seed.sql
+++ seed.sql
@@ -1,1 +1,10 @@
 --seed.sql--
+insert into five_random_gary_busey_movies
+    (title, year_released)
+values
+    ('Point Break', 1991),
+    ('A Star is Born', 1976),
+    ('Predator 2', 1990),
+    ('Big Wednesday', 1978),
+    ('Lethal Weapon', 1987)
+;
